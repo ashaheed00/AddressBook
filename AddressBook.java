a9a732dbee78b67eafc0d196061fb56115b1b6ff
ashaheed00 <ashaheed00@gmail.com>
2020-09-29 13:00:51 +0530

    Refactor >> UC2

--- AddressBook.java
+++ AddressBook.java
@@ -1,6 +1,28 @@
-package com.bl.addressbook;
+import java.util.Scanner;
 
 public class AddressBook {
 	Contact person = new Contact();
-	
+	static Scanner in = new Scanner(System.in);
+
+	public AddressBook() {
+	}
+
+	public void addNewContact() {
+		System.out.print("First Name: ");
+		person.setFirstName(in.next());
+		System.out.print("Last Name: ");
+		person.setLastName(in.next());
+		System.out.print("Address: ");
+		person.setAddress(in.next());
+		System.out.print("City: ");
+		person.setCity(in.next());
+		System.out.print("State: ");
+		person.setState(in.next());
+		System.out.print("Zip: ");
+		person.setZip(in.next());
+		System.out.println("Phone Number: ");
+		person.setPhoneNo(in.next());
+		System.out.println("Email: ");
+		person.setEmail(in.next());
+	}
 }
